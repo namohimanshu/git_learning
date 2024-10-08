b2dd28998edcde160d7b4811637ea5e870538276
Himanshu Singh <himanshusingh6612@gmail.com>
2024-10-09 01:29:32 +0530

    added

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class HelloWorld
+{
+	public static  void main (String [] args){
+	System.out.println("hellow World");
+	Scanner sc =new Scanner(System.in);
+            int a =sc.nextInt();
+	    System.out.println(a);
+	}
+}
